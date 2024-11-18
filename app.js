20c8037f4465718e3975bd5bb2cccc5c05d497f0
hadys.space <hadijones77@gmail.com>
2024-11-17 19:10:09 -0500

    Added all functionalities

--- app.js
+++ app.js
@@ -1,23 +1,42 @@
 document.addEventListener('DOMContentLoaded', function(){
     const btn = document.getElementById("searchBtn");
+    const  result = document.getElementById("result");
+    const searchHero = document.getElementById("searchHero");
 
     btn.addEventListener('click', function(event){
+            console.log("Button clicked");
             const getHeroes = new XMLHttpRequest();
+            let query = searchHero.value;
+
+            query = sanitizeSearch(query);
 
             getHeroes.onreadystatechange = function(){
 
                 if (getHeroes.readyState === XMLHttpRequest.DONE){
                     if(getHeroes.status === 200){
-                        alert(getHeroes.responseText);
+                        result.innerHTML = getHeroes.responseText;
                     }else{
-                        alert("Error: Unable to fetch superheroes.");
+                        
+                        result.innerHTML = "<p>Error: Unable to fetch superheroes.</p>";
                     }
                 }
             };
-            getHeroes.open("GET", "./superheroes.php",true);
+            getHeroes.open("GET", `./superheroes.php?query=${encodeURIComponent(query)}`,true);
             getHeroes.send();
 
         
 
     });
+
+    function sanitizeSearch(input){
+        return input.replace(/[<>&'"]/g, function(match){
+            return{
+                '<':'&lt;',
+                '>': '&gt;',
+                '&': '&amp;',
+                "'": '&#39;',
+                '"': '&quot;'
+            }[match]
+        });
+    }
 });
--- superheroes.php
+++ superheroes.php
@@ -63,10 +63,33 @@ $superheroes = [
   ], 
 ];
 
+$query = isset($_GET['query']) ? htmlspecialchars($_GET['query'], ENT_QUOTES, 'UTF-8') : '';
+
+    
+    if($query === ''){
+        echo "<ul>";
+        foreach ($superheroes as $superhero){
+            echo "<li>" . htmlspecialchars($superhero['alias'], ENT_QUOTES, 'UTF-8') . "</li>";
+        }
+        echo "</ul>";
+    }
+    else{
+        $found = false;
+
+        foreach($superheroes as $superhero){
+            if(stripos($superhero['alias'], $query) !== false || stripos($superhero['name'], $query) !== false){
+                echo "<h3>" . htmlspecialchars($superhero['alias'], ENT_QUOTES, 'UTF-8') . "</h3>";
+                echo "<h4 class='name'>" . htmlspecialchars($superhero['name'], ENT_QUOTES, 'UTF-8') . "</h4>";
+                echo "<p>" . htmlspecialchars($superhero['biography'], ENT_QUOTES, 'UTF-8') . "</p>";
+                $found = true;
+                break;
+            }   
+        }
+
+
+        if (!$found){
+            echo "<p class='notfound'>SUPERHERO NOT FOUND</p>";
+        }
+    }
 ?>
 
-<ul>
-<?php foreach ($superheroes as $superhero): ?>
-  <li><?= $superhero['alias']; ?></li>
-<?php endforeach; ?>
-</ul>
